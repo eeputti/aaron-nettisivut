8ea5f2e4d1e2501d899fdf48824dea09e99d4d38
Eelis Puro <eelispuro@gmail.com>
2026-04-08 12:32:17 +0300

    initial website

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,7 @@ export default function HomePage() {
       <main className="section">
         <div className="container page-block flow homepage-intro">
           <h1>AP Powerbuilding &amp; Demolition</h1>
-          <p className="lead">
-            kaikenlaiset hommat. kato täältä mitä oon tehny ennen ja jälkeen
-            nii osaat arvioida mitä teen.
-          </p>
-          <a className="text-link" href="/ennen-jalkeen">
-            katso ennen &amp; jälkeen
-          </a>
+          <p className="lead">kaikenlaisia hommia.</p>
           <a className="button-link" href="/tarjouspyynto">
             Jätä tarjouspyyntö
           </a>
